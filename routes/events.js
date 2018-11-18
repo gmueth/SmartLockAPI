f3c01329197a264a57f27ddbed62ca4b87080700
greg <gmueth@gmail.com>
2018-11-18 16:52:08 -0500

    Added Event model and route

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const {Event, validate} = require('../models/event');
+const {Lock} = require('../models/lock');
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
@@ -8,13 +10,19 @@ router.post('/', async (req, res) => {
     const event = new Event({ 
         lockid: req.body.lockid,
         userid: req.body.userid,
-        date: 
+        date: Date.now(),
         status: req.body.status 
     });
+
+    const lock = await Lock.findByIdAndUpdate(req.body.lockid, { status: req.body.status },
+        { new: true });
+    
+    if (!lock) return res.status(404).send('The lock with the given ID was not found.');
     
     await lock.save();
+    await event.save();
     
-    res.send(lock);
+    res.send(event);
   });
 
-module.exports = router;
+module.exports = router; 
